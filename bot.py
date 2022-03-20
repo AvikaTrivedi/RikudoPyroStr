ffe4238e31d6d2d1f49ac53381fb844259c4800b
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2022-03-20 16:40:39 +0530

    Sed

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 APP_NAME = os.environ.get("APP_NAME", None)
 API_KEY = os.environ.get("API_KEY", None)
-HU_APP = from_key(API_KEY).apps()[APP_NAME]
 
 bot = Client(":memory:",
              api_id=API_ID,
